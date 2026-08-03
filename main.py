5d0d9dba620813181f67fc76a16eb9f677ce372a
Tshiamo Nthite <nthitetshiamo0309@gmail.com>
2026-08-03 16:59:15 +0200

    Updated dev

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     num2 = float(input("Enter second number: "))
 
     print("Choose an operation:")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
     choice = input("Enter choice (1/2/3/4): ")
 
